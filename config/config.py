9fde0c33d2737574c71ac5a9b0efedc1db88a181
1minepowminx <nobusymaile@gmail.com>
2025-07-04 10:53:21 +0300

    base config init

---
+++ config/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str  # Token for accessing the Telegram bot
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    env: Env = Env()
+    env.read_env(path)
+
+    return Config(tg_bot=TgBot(token=env.str("BOT_TOKEN")))

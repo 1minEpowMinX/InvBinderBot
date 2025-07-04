5428d726e39391ba3579476f9ef57d87b99584eb
1minepowminx <nobusymaile@gmail.com>
2025-07-04 19:40:33 +0300

    Add basic bot logic with middleware and router registration

---
+++ main.py
@@ -0,0 +1,58 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from middlewares.authorization_middleware import AuthMiddleware
+from middlewares.logging_middleware import LoggingMiddleware
+from routers import (
+    access,
+    callbacks,
+    # admin,
+    public,
+)
+from config.config import Config, load_config
+from utils.auth_manager import AuthManager
+from utils.logger import setup_logger
+
+# Initialize logger and auth manager
+auth_manager = AuthManager()
+logger = setup_logger()
+
+
+async def main() -> None:
+    """Main function to configure and start the bot."""
+
+    logger.info("Starting bot")
+    config: Config = load_config()
+
+    bot = Bot(
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        token=config.tg_bot.token,
+    )
+    dp = Dispatcher()
+
+    # Register middlewares
+    logger.info("Registering middlewares")
+
+    dp.message.middleware(LoggingMiddleware(logger))
+    dp.message.middleware(AuthMiddleware(auth_manager))
+
+    dp.callback_query.middleware(LoggingMiddleware(logger))
+    dp.callback_query.middleware(AuthMiddleware(auth_manager))
+
+    # Register routers in the dispatcher
+    dp.include_router(public.router)
+    dp.include_router(access.router)
+    dp.include_router(callbacks.router)
+    # dp.include_router(admin.router)
+
+    # Drops webhook if it exists and switches to getUpdates
+    # If there were updates while the bot was off, they will be processed
+    await bot.delete_webhook(drop_pending_updates=False)
+    # start polling with only allowed updates
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

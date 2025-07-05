24177850bfa8ea35b90f1908fa2a02d181a59deb
1minepowminx <nobusymaile@gmail.com>
2025-07-05 18:13:19 +0300

    Add documentation for LoggingMiddleware and add comment to save the logger in the context of the data

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -5,6 +5,16 @@ from typing import Any, Awaitable, Callable, Dict
 
 
 class LoggingMiddleware(BaseMiddleware):
+    """
+    Middleware for logging incoming messages and callback queries in a Telegram bot.
+
+    This middleware logs the user who sent the message or callback query,
+    along with the content of the message or callback data.
+
+    Attributes:
+        logger (Logger): An instance of Logger to log messages.
+    """
+
     def __init__(self, logger: Logger):
         self.logger = logger
 
@@ -21,6 +31,8 @@ class LoggingMiddleware(BaseMiddleware):
         else:
             text = getattr(event, "text", "No text available")
         self.logger.info(f"Incoming message from {user.full_name} ({user.id}): {text}")
+
+        # Add the logger to the data context for further use in handlers
         data["logger"] = self.logger
 
         # Call the next handler in the middleware chain

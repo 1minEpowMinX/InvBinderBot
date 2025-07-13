64f9a36c3c93102b4c917c277f64107d9d36d5da
1minepowminx <nobusymaile@gmail.com>
2025-07-13 17:29:56 +0300

    Add a one-time confirmation ReplyKeyboard

---
+++ keyboards/confirmation.py
@@ -0,0 +1,18 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+def get_one_time_keyboard(keyboard_text: list[str]) -> ReplyKeyboardMarkup:
+    """
+    Creates a one-time keyboard markup with the provided text.
+
+    Args:
+        keyboard_text (list[str]): A list of strings to be used as button texts in the keyboard.
+    """
+    if not keyboard_text:
+        raise ValueError("keyboard_text must not be empty")
+
+    return ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text=btn)] for btn in keyboard_text],
+        resize_keyboard=True,
+        one_time_keyboard=True,
+    )

cd3be10464099b2677a7af09f457463ffdd54d6f
1minEpowMinX <nobusymaile@gmail.com>
2025-08-26 12:12:29 +0300

    move text to lexicon module

--- routers/public.py
+++ routers/public.py
@@ -3,15 +3,16 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup
 
+from lexicon.lexicon import get_message
+
 router = Router()
 
 
 @router.message(Command("start"))
 async def start_handler(message: Message, role: str, keyboard: ReplyKeyboardMarkup):
-    await message.answer(f"Добро пожаловать! Ваша роль: {role}", reply_markup=keyboard)
+    await message.answer(get_message(role), reply_markup=keyboard)
 
 
 @router.message(Command("help"))
 async def help_handler(message: Message, role: str, keyboard: ReplyKeyboardMarkup):
-    help_text = "Заглушка для команды /help\n\n"
-    await message.answer(help_text, reply_markup=keyboard)
+    await message.answer(get_message(f"{role}_help"), reply_markup=keyboard)

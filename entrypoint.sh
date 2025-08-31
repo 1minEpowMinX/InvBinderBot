3da984b3dd2d0b9a63ec45db6bf8bde63ca99fb1
1minepowminx <nobusymaile@gmail.com>
2025-08-31 11:05:24 +0300

    Add support for correct import of corporate CA certificates in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 
-# update CA certificates in case custom certs are added
-update-ca-certificates
+# If corporate CA certificate exists, add it to the system's trusted certificates
+if [ -f /app/certs/corp.crt ]; then
+    # split the corp.crt file into individual certificate files
+    csplit -sz /app/certs/corp.crt '/-----BEGIN CERTIFICATE-----/' '{*}'
+
+    # move the split certificate files to the system's CA directory
+    for cert in xx*; do
+        mv "$cert" /usr/local/share/ca-certificates/
+    done
+
+    # update the system's trusted certificates
+    update-ca-certificates
+fi
 
 # export environment variables from .env file if it exists
 if [ -f /app/.env ]; then

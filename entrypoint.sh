6b200dfc2a54ab35d2a381047d841d0463cad1dd
1minepowminx <nobusymaile@gmail.com>
2026-02-17 12:22:47 +0100

    Add support for corporate root certificates and SMB configuration to .env and docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -e
 
-# Check and install corporate certificate if it exists
-if [ -f /app/certs/corp.crt ]; then
-    cp /app/certs/corp.crt /usr/local/share/ca-certificates/corp.crt
+CERT_PATH="${CORP_ROOT_CA_PATH:-/app/certs/corp_root_ca.crt}"
+
+# Check and install corporate root certificate if it exists
+if [ -f "$CERT_PATH" ]; then
+    cp "$CERT_PATH" /usr/local/share/ca-certificates/corp_root_ca.crt
     update-ca-certificates
 
-    # Append the corporate certificate to certifi's bundle
-    cat /usr/local/share/ca-certificates/corp.crt >> $(python -m certifi)
+    # Append the corporate root certificate to certifi's bundle
+    cat /usr/local/share/ca-certificates/corp_root_ca.crt >> $(python -m certifi)
 
-    export REQUESTS_CA_BUNDLE=/usr/local/share/ca-certificates/corp.crt
-    export CURL_CA_BUNDLE=/usr/local/share/ca-certificates/corp.crt
+	export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+    export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+    export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+	echo "Installed $CERT_PATH as a trusted certificate"
 else
-    echo "Warning: /app/certs/corp.crt not found, skipping certificate installation"
+    echo "Warning: $CERT_PATH not found, skipping certificate installation"
 fi
 
 exec python /app/main.py
